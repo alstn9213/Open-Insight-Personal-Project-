e8446d2970df38867fc2fdccf2c4b53b3856acee
alstn9213 <alstn9213@naver.com>
2025-12-04 15:11:58 +0900

    feat:20251204 파이썬 자잘한 오류 수정

--- DATA/src/service/calculator.py
+++ DATA/src/service/calculator.py
@@ -16,11 +16,7 @@ class MarketMetricsCalculator:
   def calculate(self, store_count: int) -> dict:
     """
     점포 수를 기반으로 각종 상권 지표를 계산하여 반환합니다.
-    
-    :param store_count: API에서 조회한 실제 점포 수
-    :return: 계산된 지표들이 담긴 딕셔너리
     """
-    
     final_store_count = store_count
     is_simulated = False
     
@@ -31,24 +27,22 @@ class MarketMetricsCalculator:
     
     # 점포 수가 많을수록 경쟁으로 인해 평균 매출이 감소하는 경향을 반영
     competition_factor = final_store_count / 1000.0
-    random_variation = random.uniform(self.SALES_VARIATION_MIN, self.SALES_VARIATION_MAX)
+    random_variation = random.uniform(SALES_VARIATION_MIN, SALES_VARIATION_MAX)
     
     average_sales = int(
-      (self.BASE_SALES * random_variation) - (competition_factor * 5_000_000)
+      (BASE_SALES * random_variation) - (competition_factor * 5_000_000)
     )
     
-    growth_rate = int(
-      (self.BASE_SALES * random_variation) - (competition_factor * 5_000_000)
-    )
+    growth_rate = round(random.uniform(GROWTH_RATE_MIN, GROWTH_RATE_MAX), 2)
     
     # 폐업률 (Closing Rate) 생성
     # 매출이 기준(4천만원)보다 낮으면 폐업 위험도를 높게(가중치) 설정
     risk_factor = 1.0
-    if average_sales <= self.HIGH_SALES_THRESHOLD:
-      risk_factor = self.HIGH_RISK_FACTOR
+    if average_sales <= HIGH_SALES_THRESHOLD:
+      risk_factor = HIGH_RISK_FACTOR
       
     closing_rate = round(
-      round.uniform(self.CLOSING_RATE_MIN, self.CLOSING_RATE_MAX) * risk_factor, 2
+      round.uniform(CLOSING_RATE_MIN, CLOSING_RATE_MAX) * risk_factor, 2
     )
     
     net_growth_rate = round(growth_rate - closing_rate, 2)
@@ -68,9 +62,9 @@ class MarketMetricsCalculator:
     """
     순성장률에 따라 상권의 등급을 결정합니다. (내부 메서드)
     """
-    if net_growth_rate >= 3.0:
+    if net_growth_rate >= GRADE_GREEN_THRESHOLD:
         return "GREEN"   # 성장/추천
-    elif net_growth_rate <= -1.0:
+    elif net_growth_rate <= GRADE_RED_THRESHOLD:
         return "RED"     # 쇠퇴/위험
     else:
         return "YELLOW"  # 정체/주의
